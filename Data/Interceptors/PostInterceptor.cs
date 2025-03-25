71e7cc04a2ba87732e128c538a519b02282db2bf
Muhammad Bilal <muhammadbilal.dev9@gmail.com>
2025-03-25 16:10:02 +0500

    post soft delete code added in post interceptor

--- Data/Interceptors/PostInterceptor.cs
+++ Data/Interceptors/PostInterceptor.cs
@@ -7,34 +7,41 @@ namespace BlogPostApi.Data.Interceptors;
 
 public class PostInterceptor
 {
-     public void Apply(EntityEntry<Post> entry, bool hardDelete = false)
-        {
-            var post = entry.Entity;
-            switch (entry.State)
-            {
-                case EntityState.Added:
-                    SaveOrUpdate(post);
-                    Save(post);
-                    break;
-                case EntityState.Modified:
-                    SaveOrUpdate(post);
-                    Update(post);
-                    break;
-                case EntityState.Deleted:
-                    Delete(post, hardDelete);
-                    break;
-            }
-        }
+  public void Apply(EntityEntry<Post> entry, bool hardDelete = false)
+  {
+    var post = entry.Entity;
+    switch (entry.State)
+    {
+      case EntityState.Added:
+        SaveOrUpdate(post);
+        Save(post);
+        break;
+      case EntityState.Modified:
+        SaveOrUpdate(post);
+        Update(post);
+        break;
+      case EntityState.Deleted:
+        Delete(post, hardDelete);
+        break;
+    }
+  }
 
-        public void SaveOrUpdate(Post post)
-        {
-            if (!string.IsNullOrWhiteSpace(post.Title))
-        {
-          post.Title = char.ToUpper(post.Title.Trim()[0]) + post.Title.Trim().Substring(1);
-        }
-        }
-         public void Save(Post entry) {}
-        public void Update(Post entry) {}
-        public void Delete(Post entry, bool hardDelete = false) {}
+  public void SaveOrUpdate(Post post)
+  {
+    if (!string.IsNullOrWhiteSpace(post.Title))
+    {
+      post.Title = char.ToUpper(post.Title.Trim()[0]) + post.Title.Trim().Substring(1);
+    }
+  }
+  public void Save(Post entry) { }
+  public void Update(Post entry) { }
+  public void Delete(Post entry, bool hardDelete = false)
+  {
+
+    if (hardDelete == false)
+    {
+      entry.IsDeleted = true;
+    }
+  }
 
 }
